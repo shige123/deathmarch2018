54706134f0e01f6c8f7c2dc5254cf589d205d46f
Kohta Shigenobu <sigenobu@virgo.is.utsunomiya-u.ac.jp>
2018-05-07 09:09:21 +0900

    add tesseract-ocr

---
+++ tesseract-ocr/test-tesseract.cpp
@@ -0,0 +1,30 @@
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+int main(int argc, char* argv[])
+{
+    char *inputImageFile;
+    char *outText;
+
+    inputImageFile = argv[1];
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "eng")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    Pix *image = pixRead(inputImageFile);
+    api->SetImage(image);
+    // Get OCR result
+    outText = api->GetUTF8Text();
+    printf("OCR output:\n%s", outText);
+
+    // Destroy used object and release memory
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
+
+    return 0;
+}
