71f8286d07d515dc415b63984a7e0e6739a32d8e
sugata-yuhei <sugata@virgo.is.utsunomiya-u.ac.jp>
2018-05-16 22:19:49 +0900

    Add files via upload
    ./client 3 2 127.0.0.1 (argv[1]=連続数 argv[2]=ループカウント argv[3]= IPアドレス)
    ループはサーバからデータを受け取ってから，nonceを送信するまで

--- blockchain/client.cpp
+++ blockchain/client.cpp
@@ -36,21 +36,23 @@ void hash(string nonce){
   cout<<"double_hash:"<<endl<<double_hash<<endl;
 }
 
-bool judge_nonce(string src){
+bool judge_nonce(string src, int zero_count){
   bool judge=false;
   int i=0;
   string judge_str;
-    for(i=0;i<4;i++){
+  string zero_count_str;
+    for(i=0;i<zero_count;i++){
         judge_str.push_back(src[i]);
+        zero_count_str.push_back('0');
     }
     cout<<"judge"<<judge_str<<endl;
-    if(judge_str=="0000"){
+    if(judge_str==zero_count_str){
       judge=true;
     }
     return judge;
 }
 
-int calc_nonce(string src){
+int calc_nonce(string src, int zero_count){
   const int N = 1000*1000;
     std::vector<int> v;
     auto start = std::chrono::system_clock::now();
@@ -68,7 +70,7 @@ int calc_nonce(string src){
   cmp_str=src+std::to_string(nonce);
   picosha2::hash256_hex_string(cmp_str,result_str);
   count++;
-  hash_judge=judge_nonce(result_str);
+  hash_judge=judge_nonce(result_str,zero_count);
 }
 cout<<"count"<<count<<endl;
 cout<<"cmp_str<<"<<cmp_str<<endl;
@@ -85,7 +87,7 @@ std::cout << msec << " milli sec \n";
 
 
 
-int main()
+int main(int argc,char *argv[])
 {
 struct sockaddr_in addr;
 int sock;
@@ -100,7 +102,8 @@ int data;
  /* 接続先指定用構造体の準備 */
  addr.sin_family = AF_INET;
  addr.sin_port = htons(10050);
- addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+ //addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+ addr.sin_addr.s_addr = inet_addr(argv[3]);
 
  /* サーバに接続 */
  connect(sock, (struct sockaddr*)&addr, sizeof(addr));
@@ -115,32 +118,39 @@ int data;
 
  sendto(sock,"query.",5,0,(struct sockaddr*)&addr,sizeof(addr));
 
- memset(buf, 0, sizeof(buf));
- data = read(sock, buf, sizeof(buf));
- printf("%d, %s\n", data, buf);
-
+ int block_loop_count;
  string rcv_str;
- rcv_str=buf;
- cout<<"rcv_str: "<<rcv_str<<endl;
- bool t;
-
  int b;
- b=calc_nonce(rcv_str);
- cout<<"b:"<<b<<endl;
- nonce=to_string(b);
+ for(block_loop_count=0;block_loop_count<atoi(argv[2]);block_loop_count++){
+   memset(buf, 0, sizeof(buf));
+   data = read(sock, buf, sizeof(buf));
+   printf("%d, %s\n", data, buf);
 
- char* cstr = new char[nonce.size() + 1];
- strcpy(cstr, nonce.c_str());
- sendto(sock,cstr,nonce.size()+1,0,(struct sockaddr*)&addr,sizeof(addr));
+   //string rcv_str;
+   rcv_str=buf;
+   cout<<"rcv_str: "<<rcv_str<<endl;
 
+   //int b;
+   b=calc_nonce(rcv_str,atoi(argv[1]));
+   cout<<"b:"<<b<<endl;
+   nonce=to_string(b);
+
+   char* cstr = new char[nonce.size() + 1];
+   strcpy(cstr, nonce.c_str());
+   sendto(sock,cstr,nonce.size()+1,0,(struct sockaddr*)&addr,sizeof(addr));
+   delete[] cstr;
+ }
+
+
+/*
  memset(buf, 0, sizeof(buf));
  data = read(sock, buf, sizeof(buf));
  printf("%d, %s\n", data, buf);
 
  rcv_str=buf;
  cout<<"rcv_str: "<<rcv_str<<endl;
-
- b=calc_nonce(rcv_str);
+ cout<<"-------------ARGV-----------"<<argv[1]<<endl;
+ b=calc_nonce(rcv_str,atoi(argv[1]));
  cout<<"b:"<<b<<endl;
  nonce=to_string(b);
 
@@ -150,7 +160,9 @@ int data;
 cout<<"debug1"<<endl;
  strcpy(cstr, nonce.c_str());
  sendto(sock,cstr,nonce.size()+1,0,(struct sockaddr*)&addr,sizeof(addr));
- delete[] cstr;
+ */
+
+
 
 
  /* socketの終了 */
